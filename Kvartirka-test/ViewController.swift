30c98a2fc61550b98ae5eb7834472076df0a927e
Roman Sumorokov <iromka74@gmail.com>
2020-06-12 14:41:25 +0500

    Initial Commit

---
+++ Kvartirka-test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Kvartirka-test
+//
+//  Created by Рома Сумороков on 12.06.2020.
+//  Copyright © 2020 Рома Сумороков. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
